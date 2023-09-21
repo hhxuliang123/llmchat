c0cf5ba8212bfa280d468082628d0611a7cf9723
xl <you@example.com>
2023-09-21 16:34:01 +0800

    support picture and text prompt

--- components/Promptbar/Promptbar.tsx
+++ components/Promptbar/Promptbar.tsx
@@ -116,6 +116,28 @@ const Promptbar = () => {
     }
   }, [searchTerm, prompts]);
   
+  let addprompt = true;
+  for (var item of prompts) {
+    if (item.name == '图文并茂'){
+      addprompt = false;
+    }
+  }
+  if(addprompt){
+    prompts.push({
+      "id": "1e027be9-16db-1340-18f0-1769864867a5",
+      "name": "图文并茂",
+      "description": "生成图文并茂的内容",
+      "content": "按照后面的要求图文并茂完成任务。完成任务的过程中尽量使用插图的方式，图片插图请用markdown的格式表示，插图标签的url种使用api接口：api/showGenPicture?txt={图片的文字说明}。图片的文字说明请用英语，尽量完整，字数不超过50单词。\n\n要求如下：\n",
+      "model": {
+          "id": "gpt-4",
+          "name": "GPT-4",
+          "maxLength": 12000,
+          "tokenLimit": 4000
+      },
+      "folderId": null
+    });
+  }
+
   return (
     <PromptbarContext.Provider
       value={{
